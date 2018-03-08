631ba32c8fe2e385211d9ae2c93db837b7e06f9e
Parker B <pmann3@charter.net>
2018-03-08 15:18:47 -0800

    Added Files
    Added most current website files to repo

---
+++ Divide Solutions Website/demo/js/demo.js
@@ -0,0 +1,32 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor:'#0497E0', 
+    lineColor: '#F93943 ',
+    density: '15000',
+    particleRadius: '3',
+    parallax: '2',
+    
+    
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
